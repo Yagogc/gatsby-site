148e92058439fbde144dab86a7a78a59be83841f
Yago Gonzalez <jagogc@gmail.com>
2018-09-16 01:39:30 +0100

    Add About

--- src/components/header.js
+++ src/components/header.js
@@ -26,6 +26,14 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
     </div>
   </div>
 )
---
+++ src/pages/about.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import Layout from '../components/layout'
+
+export default class About extends Component {
+  render() {
+    return (
+      <Layout>
+        <h1>About Us</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vel
+          tellus tellus. Cras eget semper lectus. Praesent convallis mi quis
+          aliquet hendrerit. Maecenas non ligula nisi. Interdum et malesuada
+          fames ac ante ipsum primis in faucibus. Etiam fringilla, enim et
+          commodo molestie, magna orci feugiat dolor, non imperdiet neque lorem
+          nec purus. Etiam euismod nunc nisi, nec vehicula quam consequat ac. Ut
+          in risus dui. Cras elit ex, scelerisque ac faucibus id, mattis id
+          tortor.
+        </p>
+      </Layout>
+    )
+  }
+}
