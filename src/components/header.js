1f4ab1dfc4f416e47468e52a378d6d07b232a82c
Yago Gonzalez <jagogc@gmail.com>
2018-09-16 01:57:20 +0100

    Add Styled Components

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,35 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import styled from 'styled-components'
+
+const HeaderWrapper = styled.div`
+  background: rebeccapurple;
+  margin-bottom: 1.45rem;
+`
+const HeaderContainer = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1.45rem 1.0875rem;
+`
+const MenuLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`
+const MenuList = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-direction: row;
+  margin: 0;
+  > li {
+    margin-right: 10px;
+    margin-bottom: 0;
+  }
+`
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
+  <HeaderWrapper>
+    <HeaderContainer>
+      <h1>
         <Link
           to="/"
           style={{
@@ -26,16 +40,16 @@ const Header = ({ siteTitle }) => (
           {siteTitle}
         </Link>
       </h1>
-      <ul>
+      <MenuList>
         <li>
-          <Link to="/">Home</Link>
+          <MenuLink to="/">Home</MenuLink>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <MenuLink to="/about">About</MenuLink>
         </li>
-      </ul>
-    </div>
-  </div>
+      </MenuList>
+    </HeaderContainer>
+  </HeaderWrapper>
 )
 
 export default Header
